6c810ea935254ebbfb40921feca08950140f1e6c
Jose Romary Brandao <joseromarybrandao@gamil.com>
2021-06-11 20:45:51 -0300

    listagem de dados no banco - completa.

--- src/Components/CardsAnotations/index.tsx
+++ src/Components/CardsAnotations/index.tsx
@@ -1,31 +1,37 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
+
+interface Anotation {
+    id: number;
+    title: string;
+    anotation: string;
+    createdAt: string;
+}
+
 export function CardsAnotations() {
+
+    const [anotations, setAnotations] = useState<Anotation[]>([]);
+
     useEffect(() => {
         api.get('anotations')
-            .then(response => console.log(response.data))
-    },[]);
+            .then(response => setAnotations(response.data.anotations))
+    }, []);
 
     return (
         <Container>
-                <div className="cards">
-                    <div>
-                        <h3>Fobia</h3>                  
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </p>
+            <div className="cards">
+                {anotations.map(anotation => (
+                    <div key={anotation.id}>
+                        <h3>{anotation.title}</h3>
+                        <p>{anotation.anotation}</p>
                         <button type="button">Editar</button>
                         <button type="button">Excluir</button>
                     </div>
-                    <div>
-                        <h3>Ansiedade</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </p>
-                        <button type="button">Editar</button>
-                        <button type="button">Excluir</button>
-                    </div>                    
-                </div>                     
+
+                ))}
+            </div>
         </Container>
     );
 
